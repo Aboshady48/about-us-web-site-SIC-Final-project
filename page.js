ff02303ed086dcfc20154aa67de6cbbe84096ffa
Aboshady <114408147+Aboshady48@users.noreply.github.com>
2024-09-30 06:07:50 +0300

    final51000

--- page.js
+++ page.js
@@ -4,3 +4,26 @@ menuBtn.onclick = () => {
     menuBtn.classList.toggle('fa-times'); 
     navbar.classList.toggle('active');
 };
+
+var swiper = new Swiper(".card-slider", {
+
+    spaceBetween: 20,
+    grabCursor:true,
+    
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+  });
